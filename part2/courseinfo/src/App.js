fb5a4ea8a27574454d270329f9984deeeed78ee7
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-02-13 19:55:13 -0800

    declare Course component as separate module to complete exercise 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,42 +1,4 @@
-const Header = ({course}) => {
-  return (
-    <h1>{course.name}</h1>
-  )
-}
-
-const Part = ({part}) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  )
-}
-
-const Content = ({course}) => {
-  return (
-    <div>
-      {course.parts.map(part => <Part key={part.id} part={part} />)}
-    </div>
-  )
-}
-
-const Total = ({course}) => {
-  return (
-    <p>
-      <b> total of {course.parts.reduce((sum, part) => sum + part.exercises, 0)} exercises </b>
-    </p>
-  )   
-}
-
-const Course = (props) => {
-  return (
-    <div>
-      <Header course={props.course} />
-      <Content course={props.course} />
-      <Total course={props.course} />
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
