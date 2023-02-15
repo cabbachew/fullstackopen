6e215ede5cf7664e6696775d67beccf600f5115c
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-02-14 21:01:39 -0800

    implement a search field to filter list of contacts by name

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,10 +3,15 @@ import Person from './components/Person'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' },
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -14,8 +19,9 @@ const App = () => {
       name: newName,
       number: newNumber
     }
-    // Check for duplicate names
-    if (persons.some(person => person.name === newName)) {
+    // Check for duplicate names and alert if found
+    // Note: some() returns true if any of the elements in the array pass the test
+    if (persons.some(person => person.name.toLowerCase() === newName.toLowerCase())) {
       alert(`${newName} is already added to phonebook`)
       return
     }
@@ -33,9 +39,24 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const personsToShow = filter
+    ? persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+    : persons
+    
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input
+                            value={filter}
+                            onChange={handleFilterChange}
+                          />
+      </div>
+      <h2>Add a new contact</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input 
@@ -54,7 +75,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-        {persons.map(person => 
+        {personsToShow.map(person => 
           <Person key={person.name} person={person} />
         )}
     </div>
