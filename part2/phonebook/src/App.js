e4200cabbf5b832a4061770199a93015bdf773c0
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-02-17 13:26:45 -0800

    extract communication with backend into a separate module and refactor

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import personService from './services/persons'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
@@ -11,10 +11,10 @@ const App = () => {
   const [filter, setFilter] = useState('')
     
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
       })
   }, []) // [] means this effect is run once when the component is first rendered
 
@@ -30,11 +30,11 @@ const App = () => {
       alert(`${newName} is already added to phonebook`)
       return
     }
-    // Save new person and reset input field
-    axios
-      .post('http://localhost:3001/persons', personObject)
-      .then(response => {
-        setPersons(persons.concat(response.data))
+    // Save new person and reset input fields
+    personService
+      .create(personObject)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
         setNewName('')
         setNewNumber('')
       })
