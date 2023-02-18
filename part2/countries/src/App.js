a3a2e883e2f380ec11ee3078b0d18c30cf175c5b
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-02-18 12:11:55 -0800

    render a filtered list of countries

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -4,14 +4,34 @@ import Filter from './components/Filter';
 
 const App = () => {
   const [filter, setFilter] = useState('');
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    countryService.getAll().then((initialCountries) => {
+      setCountries(initialCountries);
+    });
+  }, []);
+
+  const countriesToShow = filter
+    ? countries.filter((country) =>
+        country.name.common.toLowerCase().includes(filter.toLowerCase())
+      )
+    : countries;
 
   const handleFilterChange = (event) => {
     setFilter(event.target.value);
   };
 
+  // Log the number of countries to show
+  console.log('number of countries to show', countriesToShow.length);
+
   return (
     <div>
       <Filter filter={filter} handleFilterChange={handleFilterChange} />
+      {countriesToShow.map((country) => (
+        // cca3 is a unique identifier for each country
+        <div key={country.cca3}>{country.name.common}</div>
+      ))} 
     </div>
   );
 }
