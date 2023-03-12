71238726062e140860b1e9a86b753c3fdb1acd14
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-03-11 22:45:23 -0800

    refactor anecdotes and filter reducers using createSlice function

--- part6/redux-anecdotes/src/reducers/filterReducer.js
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,17 +1,14 @@
-const filterReducer = (state = '', action) => {
-  switch (action.type) {
-    case 'FILTER':
+import { createSlice } from "@reduxjs/toolkit"
+
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState: '',
+  reducers: {
+    filterAnecdotes(state, action) {
       return action.payload
-    default:
-      return state
+    }
   }
-}
+})
 
-export const filterAnecdotes = (filter) => {
-  return {
-    type: 'FILTER',
-    payload: filter
-  }
-}
-
-export default filterReducer
+export const { filterAnecdotes } = filterSlice.actions
+export default filterSlice.reducer
