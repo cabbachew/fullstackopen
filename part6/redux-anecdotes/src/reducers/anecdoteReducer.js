cb53662627186721e5040750a720181590df6a64
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-03-11 21:42:38 -0800

    define a new reducer and action creator for filter state

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -48,16 +48,13 @@ export const voteAnecdote = (id) => {
   }
 }
 
-const generateId = () =>
-  Number((Math.random() * 1000000).toFixed(0))
-
 export const createAnecdote = (content) => {
   return {
     type: 'NEW_ANECDOTE',
     payload: {
       content,
       votes: 0,
-      id: generateId()
+      id: getId()
     }
   }
 }
