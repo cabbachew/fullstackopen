56aae4c9eab0b4634cc522f4d2d15033452f4185
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-03-12 14:19:34 -0700

    render notification or filter conditionally based on state

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,13 +1,19 @@
+import { useSelector } from 'react-redux'
 import Filter from './components/Filter'
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
 
 const App = () => {
+  const notification = useSelector(state => state.notification)
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      <Filter />
+      {notification
+        ? <Notification notification={notification} />
+        : <Filter />
+      }
       <AnecdoteList />
       <AnecdoteForm />
     </div>
--- part6/redux-anecdotes/src/components/Notification.js
+++ part6/redux-anecdotes/src/components/Notification.js
@@ -1,12 +1,17 @@
-const Notification = () => {
+const Notification = ({ notification }) => {
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1
   }
+
+  if (notification === '') {
+    return null
+  }
+
   return (
     <div style={style}>
-      render here notification...
+      {notification}
     </div>
   )
 }
