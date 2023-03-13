df058b3e5ed95d0439b166097b4e5cb45e154924
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-03-13 15:07:06 -0700

    check for query error state

--- part6/query-anecdotes/src/App.js
+++ part6/query-anecdotes/src/App.js
@@ -34,6 +34,10 @@ const App = () => {
     return <div>loading data...</div>
   }
 
+  if (result.isError) {
+    return <div>anecdote service not available due to problems in server</div>
+  }
+
   const anecdotes = result.data
 
   return (
