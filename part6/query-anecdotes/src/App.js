4b4231cc9d14d425c2394aa2f98f4bd4ac2b4e81
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-03-13 16:08:38 -0700

    move logic to handle post request via mutation into anecdote form component

--- part6/query-anecdotes/src/App.js
+++ part6/query-anecdotes/src/App.js
@@ -1,18 +1,10 @@
-
 import { useQuery, useMutation, useQueryClient } from 'react-query'
-import { getAnecdotes, createAnecdote, updateAnecdote } from './requests'
+import { getAnecdotes, updateAnecdote } from './requests'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
 const App = () => {
   const queryClient = useQueryClient()
-
-  const newAnecdoteMutation = useMutation(createAnecdote, {
-    onSuccess: (newAnecdote) => {
-      const anecdotes = queryClient.getQueryData('anecdotes')
-      queryClient.setQueryData('anecdotes', [...anecdotes, newAnecdote])
-    }
-  })
 
   const updateAnecdoteMutation = useMutation(updateAnecdote, {
     onSuccess: (updatedAnecdote) => {
@@ -20,13 +12,6 @@ const App = () => {
       queryClient.setQueryData('anecdotes', anecdotes.map(anecdote => anecdote.id === updatedAnecdote.id ? updatedAnecdote : anecdote))
     }
   })
-
-  const onCreate = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    newAnecdoteMutation.mutate(content)
-  }
 
   const handleVote = (anecdote) => {
     console.log('vote')
@@ -57,7 +42,7 @@ const App = () => {
       <h3>Anecdote app</h3>
     
       <Notification />
-      <AnecdoteForm onCreate={onCreate} />
+      <AnecdoteForm />
     
       {anecdotes.map(anecdote =>
         <div key={anecdote.id}>
--- part6/query-anecdotes/src/components/AnecdoteForm.js
+++ part6/query-anecdotes/src/components/AnecdoteForm.js
@@ -1,11 +1,22 @@
-const AnecdoteForm = ({ onCreate }) => {
+import { useMutation, useQueryClient } from 'react-query'
+import { createAnecdote } from '../requests'
 
-//   const onCreate = (event) => {
-//     event.preventDefault()
-//     const content = event.target.anecdote.value
-//     event.target.anecdote.value = ''
-//     console.log('new anecdote')
-// }
+const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+
+  const newAnecdoteMutation = useMutation(createAnecdote, {
+    onSuccess: (newAnecdote) => {
+      const anecdotes = queryClient.getQueryData('anecdotes')
+      queryClient.setQueryData('anecdotes', [...anecdotes, newAnecdote])
+    }
+  })
+
+  const onCreate = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    newAnecdoteMutation.mutate(content)
+  }
 
   return (
     <div>
