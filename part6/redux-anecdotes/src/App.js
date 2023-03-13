bc7b85a81fd5b9c37e3d36c8a93567c40d1e902c
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-03-12 17:24:34 -0700

    create service module and define effect to fetch anecdotes from server

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,11 +1,20 @@
-import { useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import Filter from './components/Filter'
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import anecdoteService from './services/anecdotes'
+import { setAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
   const notification = useSelector(state => state.notification)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdoteService.getAll().then(anecdotes =>
+      dispatch(setAnecdotes(anecdotes))
+    )
+  }, [dispatch])
 
   return (
     <div>
