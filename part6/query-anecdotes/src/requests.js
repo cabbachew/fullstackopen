82f465a79e7b2ccea186fb7ced95208389e637e2
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-03-13 14:31:58 -0700

    extend requests module for anecdote creation

--- part6/query-anecdotes/src/requests.js
+++ part6/query-anecdotes/src/requests.js
@@ -4,3 +4,8 @@ export const getAnecdotes = async () => {
   const response = await axios.get('http://localhost:3001/anecdotes')
   return response.data
 }
+
+export const createAnecdote = async (content) => {
+  const response = await axios.post('http://localhost:3001/anecdotes', { content, votes: 0 })
+  return response.data
+}
