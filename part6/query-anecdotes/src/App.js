a8b2af853ec121fd914265b17daa389698168416
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-03-13 16:13:07 -0700

    extract anecdote list into a separate component and refactor

--- part6/query-anecdotes/src/App.js
+++ part6/query-anecdotes/src/App.js
@@ -1,41 +1,21 @@
-import { useQuery, useMutation, useQueryClient } from 'react-query'
-import { getAnecdotes, updateAnecdote } from './requests'
+import { useQuery } from 'react-query'
+import { getAnecdotes } from './requests'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
-  const queryClient = useQueryClient()
-
-  const updateAnecdoteMutation = useMutation(updateAnecdote, {
-    onSuccess: (updatedAnecdote) => {
-      const anecdotes = queryClient.getQueryData('anecdotes')
-      queryClient.setQueryData('anecdotes', anecdotes.map(anecdote => anecdote.id === updatedAnecdote.id ? updatedAnecdote : anecdote))
-    }
-  })
-
-  const handleVote = (anecdote) => {
-    console.log('vote')
-    const updatedAnecdote = {
-      ...anecdote,
-      votes: anecdote.votes + 1
-    }
-    updateAnecdoteMutation.mutate(updatedAnecdote)
-  }
-
-  const result = useQuery('anecdotes', getAnecdotes, {
+  const { isLoading, isError, data } = useQuery('anecdotes', getAnecdotes, {
     refetchOnWindowFocus: false
   })
-  console.log(result)
 
-  if (result.isLoading) {
+  if (isLoading) {
     return <div>loading data...</div>
   }
 
-  if (result.isError) {
+  if (isError) {
     return <div>anecdote service not available due to problems in server</div>
   }
-
-  const anecdotes = result.data
 
   return (
     <div>
@@ -43,18 +23,7 @@ const App = () => {
     
       <Notification />
       <AnecdoteForm />
-    
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => handleVote(anecdote)}>vote</button>
-          </div>
-        </div>
-      )}
+      <AnecdoteList anecdotes={data} />
     </div>
   )
 }
---
+++ part6/query-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,40 @@
+import { useMutation, useQueryClient } from 'react-query'
+import { updateAnecdote } from '../requests'
+
+const AnecdoteList = ({ anecdotes }) => {
+  const queryClient = useQueryClient()
+
+  const updateAnecdoteMutation = useMutation(updateAnecdote, {
+    onSuccess: (updatedAnecdote) => {
+      const anecdotes = queryClient.getQueryData('anecdotes')
+      queryClient.setQueryData('anecdotes', anecdotes.map(anecdote => anecdote.id === updatedAnecdote.id ? updatedAnecdote : anecdote))
+    }
+  })
+
+  const handleVote = (anecdote) => {
+    console.log('vote')
+    const updatedAnecdote = {
+      ...anecdote,
+      votes: anecdote.votes + 1
+    }
+    updateAnecdoteMutation.mutate(updatedAnecdote)
+  }
+
+  return (
+    <>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default AnecdoteList
