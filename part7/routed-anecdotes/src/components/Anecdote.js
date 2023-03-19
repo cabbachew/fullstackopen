239db1b8d9ddadaecf3c832ee24cee004e0d05be
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-03-19 15:25:09 -0400

    extract components into separate modules

---
+++ part7/routed-anecdotes/src/components/Anecdote.js
@@ -0,0 +1,14 @@
+const Anecdote = ({ anecdote }) => {
+  // const id = useParams().id
+  // const anecdote = anecdotes.find(a => Number(a.id) === Number(id))
+
+  return (
+    <div>
+      <h2>{anecdote.content} by {anecdote.author}</h2>
+      <p>has {anecdote.votes} votes</p>
+      <p>for more info see <a href={anecdote.info}>{anecdote.info}</a></p>
+    </div>
+  )
+}
+
+export default Anecdote
